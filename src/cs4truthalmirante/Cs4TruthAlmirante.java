f568274201d98483de94f172a9caeb1022769929
TRUTH <TRUTH@172.22.16.67>
2023-09-05 14:13:45 +0800

    initial

--- src/cs4truthalmirante/Cs4TruthAlmirante.java
+++ src/cs4truthalmirante/Cs4TruthAlmirante.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs4truthzafranco;
+package cs4truthalmirante;
 
 /**
  *
  * @author TRUTH
  */
-public class Cs4TruthZafranco {
+public class Cs4TruthAlmirante {
 
     /**
      * @param args the command line arguments
