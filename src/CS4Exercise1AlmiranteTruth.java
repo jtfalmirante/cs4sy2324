8b22b7bfd6ad784e6bbe41af4d982d9de0e9a3e4
TRUTH <TRUTH@172.22.16.67>
2023-09-12 14:28:11 +0800

    Ex2 progress

---
+++ src/CS4Exercise1AlmiranteTruth.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TRUTH
+ */
+public class CS4Exercise1AlmiranteTruth {
+    String name1 = "Rever";
+    String status1 = "prince";
+    int age1 = 18;
+    
+    String name2 = "Sevik";
+    String status2 = "scholar";
+    int age2 = 26;
+    
+    String name3 = "Alocer";
+    String status3 = "reaper";
+    int age3 = 20;
+}
